bddaf393f8400cfd070f9447716314669363455d
DannyTran1806 <dannytran1806@gmail.com>
2025-10-09 19:25:45 +0200

    total of puchases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double total = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("done")) break;
 
+            String[] entries = input.split(" ");
+            String pricePart = entries[entries.length - 1];
+            double price = Double.parseDouble(pricePart.replace("$", ""));
+            total += price;
+        }
+
+        System.out.println("Total: $" + total);
+
+        scanner.close();
     }
 }
