44e8abee0580f6ef6eed3fe352533043528a285c
DannyTran1806 <dannytran1806@gmail.com>
2025-10-10 07:21:40 +0200

    show the list of all purchases

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double total = 0;
-        while (true) {
+        List<String> purchases = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("done")) break;
+            purchases.add(input);
+
 
             String[] entries = input.split(" ");
             String pricePart = entries[entries.length - 1];
             double price = Double.parseDouble(pricePart.replace("$", ""));
             total += price;
         }
-
-        System.out.println("Total: $" + total);
+        for(String purchase : purchases) {
+            System.out.println(purchase);
+        }
+        System.out.println();
+        System.out.printf(Locale.US,"Total: $%.2f%n" , total);
 
         scanner.close();
     }
