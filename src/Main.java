b1e9ba5bd8d22cd3753c85acd2e1100de928d9af
DannyTran1806 <dannytran1806@gmail.com>
2025-10-10 16:16:52 +0200

    added addIncome feature

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Choose your action:");
+
+    }
+
+    public static double addIncome(){
+        System.out.println("Enter income:");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    public static void countPurchases(){
         Scanner scanner = new Scanner(System.in);
         double total = 0;
         List<String> purchases = new ArrayList<>();
@@ -12,13 +23,12 @@ public class Main {
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             purchases.add(input);
-
-
             String[] entries = input.split(" ");
             String pricePart = entries[entries.length - 1];
             double price = Double.parseDouble(pricePart.replace("$", ""));
             total += price;
         }
+
         for(String purchase : purchases) {
             System.out.println(purchase);
         }
